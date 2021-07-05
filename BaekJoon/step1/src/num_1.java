6de7c79d7c9ea8fc6fa7f38ebbf026aa3f52f7ee
ayeonee <zn_yeon@naver.com>
2021-07-05 22:39:47 +0900

    day 1 - step1 clear

---
+++ BaekJoon/step1/src/num_1.java
@@ -0,0 +1,5 @@
+public class num_1 {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
---
+++ BaekJoon/step1/src/num_3.java
@@ -0,0 +1,10 @@
+public class num_3 {
+    public static void main(String[] args) {
+        System.out.println(
+                "\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|"
+        );
+    }
+}
